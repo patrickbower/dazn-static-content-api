41faea6f5948ca97d5d9a02465a65cb1d0084908
Patrick Bower <bower.patrick@gmail.com>
2021-05-26 15:33:09 +0100

    feat: building query string

--- api.js
+++ api.js
@@ -1,24 +1,24 @@
-module.exports = {
+const api = {
   rails: {
     url: "https://rails.discovery.indazn.com/ca/v8/rails",
-    default: {
-      country: "ca",
+    queryPrams: {
+      country: null,
       groupId: "home",
     },
   },
   tiles: {
     url: "https://rail.discovery.indazn.com/eu/v3/Rail",
-    default: {
-      id: undefined,
-      country: "ca",
+    queryPrams: {
+      id: null,
+      country: null,
       languageCode: "en",
       params: "PageType:Home;ContentType:None",
     },
   },
   image: {
     url: "https://image.discovery.indazn.com/eu/v2/eu/image",
-    default: {
-      id: undefined,
+    queryPrams: {
+      id: null,
       quality: 85,
       width: 668,
       height: 374,
@@ -26,3 +26,5 @@ module.exports = {
     },
   },
 };
+
+export default api;
--- index.js
+++ index.js
@@ -1,13 +1,57 @@
-const fetch = require("node-fetch");
-const api = require("./api");
+import api from "./api.js";
 
 // fetch("https://rails.discovery.indazn.com/ca/v8/rails?country=ca&groupId=home")
 //   .then((res) => res.text())
 //   .then((body) => console.log(body));
 
-const requestBuilder = (country) => {
-  console.log(country);
+/**
+ * getFormData
+ * serialize form data
+ *
+ * @param {object} form
+ * @returns object
+ */
+const getFormData = (form) => {
+  const formData = new FormData(form);
+  const formObject = Object.fromEntries(formData);
+  return formObject;
 };
 
-var args = process.argv.slice(2);
-console.log(args);
+const buildUrlQueryString = (queryPrams, formData) => {
+  const queryPramsKeys = Object.keys(queryPrams);
+  const formDataKeys = Object.keys(formData);
+  const intersection = queryPramsKeys.filter((element) =>
+    formDataKeys.includes(element)
+  );
+  const compiled = { ...queryPrams, ...formData };
+  return compiled;
+};
+
+const buildUrl = (api, formData) => {
+  const { url, queryPrams } = api;
+  const queryString = buildUrlQueryString(queryPrams, formData);
+
+  console.log("queryString", queryString);
+  // return `${url}?${queryString}`;
+};
+
+/**
+ * initRequest
+ * Constructor function
+ *
+ * @param {object} event
+ */
+const initRequest = (event) => {
+  event.preventDefault();
+  const formData = getFormData(event.target);
+  const railsQueryString = buildUrl(api.rails, formData);
+  // console.log(railsQueryString);
+  // print(request);
+};
+
+// const print = (body) => {
+//   document.querySelector("#print").textContent = body;
+// };
+
+const form = document.querySelector("#form");
+form.addEventListener("submit", initRequest);
