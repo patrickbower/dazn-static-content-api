43147859a6a7071cdc76a0cc27751f3528b0653f
Patrick Bower <bower.patrick@gmail.com>
2021-05-27 14:51:44 +0100

    refactor: change approach remove unnecessary abstraction. Logic return all page data raw

--- api.js
+++ api.js
@@ -1,30 +1,29 @@
-const api = {
-  rails: {
-    url: "https://rails.discovery.indazn.com/ca/v8/rails",
-    queryPrams: {
-      country: null,
-      groupId: "home",
-    },
-  },
-  tiles: {
-    url: "https://rail.discovery.indazn.com/eu/v3/Rail",
-    queryPrams: {
-      id: null,
-      country: null,
-      languageCode: "en",
-      params: "PageType:Home;ContentType:None",
-    },
-  },
-  image: {
-    url: "https://image.discovery.indazn.com/eu/v2/eu/image",
-    queryPrams: {
-      id: null,
-      quality: 85,
-      width: 668,
-      height: 374,
-      format: "jpg",
-    },
-  },
+const prams = {
+  country: undefined,
+  rail_id: undefined,
+  image_id: undefined,
 };
 
-export default api;
+const railsSchema = (country) => {
+  return `https://rails.discovery.indazn.com/ca/v8/rails?country=${country}&groupId=home`;
+};
+
+const rail = (rail_id, country) => {
+  return `https://rail.discovery.indazn.com/eu/v3/Rail?id=${rail_id}&country=${country}&languageCode=en&params=PageType:Home;ContentType:None`;
+};
+
+const image = (
+  image_id,
+  image_quality = 85,
+  image_width = 668,
+  image_height = 374,
+  image_format = "jpg"
+) => {
+  return `https://image.discovery.indazn.com/eu/v2/eu/image/?id=${image_id}&quality=${image_quality}&width=${image_width}&height=${image_height}&resizeAction=fill&verticalAlignment=top&format=${image_format}`;
+};
+
+export default { prams, railsSchema, rail, image };
+
+// https://rails.discovery.indazn.com/ca/v8/rails?country=br&groupId=home
+// https://rail.discovery.indazn.com/eu/v3/Rail?id=Scheduled&country=br&languageCode=en&params=PageType:Home;ContentType:None
+// https://image.discovery.indazn.com/eu/v2/eu/image/?id=78006341583_image-header_pRow_1594209231000&quality=85&width=668&height=374&resizeAction=fill&verticalAlignment=top&format=jpg
--- index.js
+++ index.js
@@ -1,59 +1,6 @@
 import Api from "./api.js";
 
 /**
- * constructProps - merge custom data with defaults
- * @param {object} queryPrams
- * @param {object} customValues
- * @returns {object}
- */
-const constructProps = (queryPrams, customValues) => {
-  for (const property in queryPrams) {
-    if (queryPrams[property] === null) {
-      queryPrams[property] = customValues[property];
-    }
-  }
-  console.log("two", queryPrams);
-  return queryPrams;
-};
-
-/**
- * buildPrams - construct query string from data
- * @param {object} properties -
- * @returns {string}
- */
-const buildPrams = (properties) => {
-  let queryString = "";
-  for (const key in properties) {
-    queryString += `${key}=${properties[key]}&`;
-  }
-  return queryString.slice(0, -1);
-};
-
-/**
- * buildUrl - construct a url from defaults and form inputted data
- * @param {object} defaultValues - default settings from json
- * @param {object} customValues - form inputted values
- * @returns {string}
- */
-const buildUrl = (defaultValues, customValues) => {
-  const { url, queryPrams } = defaultValues;
-  const properties = constructProps(queryPrams, customValues);
-  const queryString = buildPrams(properties);
-  return `${url}?${queryString}`;
-};
-
-const buildMultiUrls = (defaultValues, customValues, listArray) => {
-  const urlList = listArray.map((item) => {
-    const listValues = constructProps(defaultValues.queryPrams, {
-      id: item.Id,
-    });
-    // const url = buildUrl(listValues, customValues);
-    // console.log(url);
-  });
-  return urlList;
-};
-
-/**
  * getRequest - fetch api
  * @param {string} url
  * @returns {object} json
@@ -64,17 +11,33 @@ const getRequest = async (url) => {
   return data;
 };
 
+const mergeObjects = (a, b) => {
+  const merged = { ...a, ...b };
+  return merged;
+};
+
+const getRailsData = async (railsSchema, prams) => {
+  const getRailData = railsSchema.map(async (rail) => {
+    const railsUrl = Api.rail(rail.Id, prams.country);
+    const railData = await getRequest(railsUrl).then((data) => data);
+    return railData;
+  });
+  const railsData = await Promise.all(getRailData);
+  return railsData;
+};
+
 /**
  * processRequest - controller function
  * @param {object} event - handle form submission
  */
 const processRequest = async (event) => {
   event.preventDefault();
-  const customValues = serializeFormData(event.target);
-  const railsUrl = buildUrl(Api.rails, customValues);
-  const railsList = await getRequest(railsUrl).then((data) => data.Rails);
-  const tileUrlList = buildMultiUrls(Api.tiles, customValues, railsList);
-  // console.log(tileUrlList);
+  const customPrams = serializeFormData(event.target);
+  const prams = mergeObjects(Api.prams, customPrams);
+  const railsUrl = Api.railsSchema(prams.country);
+  const railsSchema = await getRequest(railsUrl).then((data) => data.Rails);
+  const railsData = await getRailsData(railsSchema, prams);
+  console.log(railsData);
 };
 
 /**
@@ -83,17 +46,13 @@ const processRequest = async (event) => {
  * @returns {object}
  */
 const serializeFormData = (form) => {
-  const customValues = new FormData(form);
-  const formObject = Object.fromEntries(customValues);
-  return formObject;
+  const formData = new FormData(form);
+  const customPrams = Object.fromEntries(formData);
+  return customPrams;
 };
 
 /**
  * Setup form submission handling
  */
 const form = document.querySelector("#form");
-form.addEventListener("submit", processRequest);
-
-// https://rails.discovery.indazn.com/ca/v8/rails?country=br&groupId=home
-// https://rail.discovery.indazn.com/eu/v3/Rail?id=Scheduled&country=br&languageCode=en&params=PageType:Home;ContentType:None
-// https://image.discovery.indazn.com/eu/v2/eu/image/?id=78006341583_image-header_pRow_1594209231000&quality=85&width=668&height=374&resizeAction=fill&verticalAlignment=top&format=jpg
+form.addEventListener("submit", processRequest, false);
